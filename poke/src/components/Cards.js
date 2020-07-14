be91faf6925025468a435fa91a2cbc08acc5962a
laura <lauradondiego@gmail.com>
2020-07-14 14:31:38 -0400

    added craco for semantic ui

--- poke/src/components/Cards.js
+++ poke/src/components/Cards.js
@@ -5,18 +5,13 @@ import cardImage from "../assets/cardImage.jpg"
 
 export const CardExampleCard = ({ char }) => (
     <Card>
-        <Image src={cardImage} style={{ width: "100px" }} />{" "}
+        <Image src={cardImage} style={{ width: "100px" }} />
         <div className="name">
             <h2>poke Name: {char.name}</h2>
         </div>
-        <div className="birth">
-            <p>Birth Year: {char.birth_year}</p>
+        <div className="url">
+            <p>Birth Year: {char.url}</p>
         </div>
-        <div className="eyecolor">
-            <p>Eye Color: {char.eye_color}</p>
-        </div>
-        <div className="mass">
-            <p>Mass: {char.mass}</p>
-        </div>
+
     </Card>
 );
--- poke/src/components/PokeData.js
+++ poke/src/components/PokeData.js
@@ -3,4 +3,25 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 import { cardExampleCard } from "./Cards"
-import { Card, Image } from "semantic-ui-react";
+
+const PokeData = () => {
+    const [data, setData] = useState([]);
+    console.log("data", data)
+
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=1`).then(res => {
+            setData(res.data.results);
+            console.log("get response data", res.data)
+            console.log("characters object", setData)
+        });
+    }, [])
+    //[] above to only load once
+
+    return (
+        <div className="App">
+            {data && data.map(char => <cardExampleCard char={char} />)}
+        </div>
+    )
+}
+
+export default PokeData
